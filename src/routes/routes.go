4a6b71dfda439b89b25bd97a1ecfe4df137e3caa
rahat.adnan <sthep90@Gmail.com>
2024-07-16 00:47:12 +0600

    prject initialized

---
+++ src/routes/routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	_ "Ecom/src/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func userRoutes(requestRoutes *gin.Engine) {
+	requestRoutes.POST("/user/signup", controllers.userSignUp())
+	requestRoutes.POST("/user/login", controllers.userLogin())
+	requestRoutes.POST("/admin/addProduct", controllers.adminAddProduct())
+	requestRoutes.GET("/user/ProductView", controllers.userProductView())
+	requestRoutes.GET("/user/search", controllers.userSearch())
+
+}
